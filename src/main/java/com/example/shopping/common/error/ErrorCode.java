3f3251c624b5c8a869949b751922917187054708
postwo <codo7717@naver.com>
2025-12-08 22:22:14 +0900

    errorcode 수정

--- src/main/java/com/example/shopping/common/error/ErrorCode.java
+++ src/main/java/com/example/shopping/common/error/ErrorCode.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode implements ErrorCodeIfs {
-    //외부코등 ,내부코드(이거는 내가 서비스에 사용할 에러코드 이다,HttpStatus코드 와 일치 할 수도 있지만 일치 하지 않을수도 있다) ,그냥 내용
-    OK(200, 200, "성공"),
+    //외부코드 ,내부코드(이거는 내가 서비스에 사용할 에러코드 이다,HttpStatus코드 와 일치 할 수도 있지만 일치 하지 않을수도 있다) ,그냥 내용
+    OK(HttpStatus.OK.value(), 200, "성공"),
 
     BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), 400, "잘못된 요청"),
 
@@ -19,7 +19,7 @@ public enum ErrorCode implements ErrorCodeIfs {
     ;
 
     // 변형이 안일어나게 하기 위해 final 붙임
-    private final Integer httpStatusCode;
-    private final Integer errorCode;
-    private final String description;
+    private final Integer httpStatusCode; //클라이언트에게 응답 시 사용하는 외부 표준 코드
+    private final Integer errorCode; //오류를 상세하게 구분하기 위한 내부 고유 코드
+    private final String description; //설명
 }
--- src/main/java/com/example/shopping/common/error/MemberErrorCode.java
+++ src/main/java/com/example/shopping/common/error/MemberErrorCode.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @Getter
 public enum MemberErrorCode implements ErrorCodeIfs{
 
-    USER_NOT_FOUND(400 , 1404 , "사용자를 찾을 수 없음."),
-
+    MEMBER_NOT_FOUND(400 , 1404 , "사용자를 찾을 수 없음."),
+    MEMBER_ALREADY_EXISTS(409, 1409, "이미 사용 중인 아이디입니다.")
     ;
 
     private final Integer httpStatusCode;
