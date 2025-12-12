323d4c79ca26c24def7a53ddd4eadfddef2ad52a
postwo <codo7717@naver.com>
2025-12-12 22:36:01 +0900

    controller 메서드 이름 변경

--- src/main/java/com/example/shopping/controller/MemberController.java
+++ src/main/java/com/example/shopping/controller/MemberController.java
@@ -1,9 +1,11 @@
 package com.example.shopping.controller;
 
 import com.example.shopping.common.api.Api;
-import com.example.shopping.dto.member.MemberRequest;
+import com.example.shopping.dto.member.LoginRequest;
+import com.example.shopping.dto.member.RegisterRequest;
 import com.example.shopping.entity.member.Member;
 import com.example.shopping.service.implement.MemberServiceImplement;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +20,14 @@ public class MemberController {
     private final MemberServiceImplement memberServiceImplement;
 
     @PostMapping("/register")
-    public Api<Member> registerMember(@RequestBody MemberRequest request){
+    public Api<Member> registerMember(@RequestBody RegisterRequest request){
         Member member = memberServiceImplement.registerMember(request);
+        return Api.OK(member);
+    }
+
+    @PostMapping("/login")
+    public Api<Member> loginMember(@RequestBody LoginRequest request, HttpServletResponse response){
+        Member member = memberServiceImplement.login(request,response);
         return Api.OK(member);
     }
 }
